054e5392fcf2862d3794d6bfa6fb2b2436887673
Michael Stromberg <mstromberg@illumina.com>
2017-08-14 21:37:51 -0700

    Updated to .NET core 2.0

--- Securities/IPrice.cs
+++ Securities/IPrice.cs
@@ -6,9 +6,6 @@ namespace Securities
     public interface IPrice
     {
         DateTime Date { get; }
-        double Open { get; }
-        double High { get; }
-        double Low { get; }
         double Close { get; }
         void Write(BinaryWriter writer);
     }
