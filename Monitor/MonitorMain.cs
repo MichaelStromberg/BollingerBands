f31f1e2552dcb1a7768eb4c8e71e96b7da82c0a9
Michael Stromberg <mstromberg@illumina.com>
2017-08-19 07:50:05 -0700

    Updated the monitor

--- Monitor/MonitorMain.cs
+++ Monitor/MonitorMain.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using Analyzers.Common;
 using Securities;
@@ -16,7 +17,7 @@ namespace Monitor
             }
 
             var inputDir = args[0];
-            var symbols  = new[] {"ILMN", "JKD", "SCHD"};
+            var symbols  = new[] {"ILMN", "JKD", "RWW", "IYM", "IDU"};
 
             foreach (var symbol in symbols)
             {
@@ -25,7 +26,7 @@ namespace Monitor
                 var tsvPath  = Path.Combine(inputDir, $"{symbol}.tsv");
 
                 var parameters = LoadParameters(bbPath);
-                var security   = LoadSecurity(dataPath).LastTwoWeeks(parameters.NumPeriods);
+                var security   = LoadSecurity(dataPath, false).LastTwoWeeks(parameters.NumPeriods);
 
                 var bollingerBand = new BollingerBand(parameters.NumPeriods, parameters.NumStddevs);
 
@@ -33,19 +34,21 @@ namespace Monitor
                 {
                     writer.WriteLine("date\tbollinger_low\tlow\tclose\thigh\tbollinger_high");
 
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine(security.Symbol);
-                    Console.WriteLine(parameters);
+                    Console.ResetColor();
 
-                    Console.WriteLine("      date  bb_low     low   close    high bb_high");
-                    //Console.WriteLine("2017-07-25 $148.02 $148.65 $148.75 $149.06 $149.04");
+                    Console.WriteLine($"Periods: {parameters.NumPeriods}, Stddevs: {parameters.NumStddevs:0.0000}, Buy: {parameters.BuyTargetPercent * 100.0:0.000} %, Sell: {parameters.SellTargetPercent * 100.0:0.000} %\n");
 
+                    DisplayColor("      date  bb_low     low   close    high bb_high\n", true, ConsoleColor.Yellow);
+ 
                     foreach (var price in security.Prices)
                     {
                         bollingerBand.Recalculate(price);
 
                         if (bollingerBand.IsCalculated)
                         {
-                            Console.WriteLine($"{price.Date:yyyy-MM-dd} {bollingerBand.LowerBandPrice * parameters.BuyTargetPercent,7:C} {price.Low,7:C} {price.Close,7:C} {price.High,7:C} {bollingerBand.UpperBandPrice * parameters.SellTargetPercent,7:C}");
+                            DisplayPrice(price, bollingerBand, parameters);                            
                             writer.WriteLine($"{price.Date:yyyy-MM-dd}\t{bollingerBand.LowerBandPrice * parameters.BuyTargetPercent:C}\t{price.Low:C}\t{price.Close:C}\t{price.High:C}\t{bollingerBand.UpperBandPrice * parameters.SellTargetPercent:C}");
                         }
                     }
@@ -54,6 +57,36 @@ namespace Monitor
 
                 Console.WriteLine();
             }
+        }
+
+        private static void DisplayPrice(IPrice price, BollingerBand bollingerBand, Parameters parameters)
+        {
+            var bollingerLow  = bollingerBand.LowerBandPrice * parameters.BuyTargetPercent;
+            var bollingerHigh = bollingerBand.UpperBandPrice * parameters.SellTargetPercent;
+
+            const ConsoleColor lowEventColor  = ConsoleColor.Red;
+            const ConsoleColor highEventColor = ConsoleColor.Green;
+
+            bool hasLowEvent  = price.Low <= bollingerLow;
+            bool hasHighEvent = price.High >= bollingerHigh;
+
+            DisplayColor($"{price.Date:yyyy-MM-dd} ", true, ConsoleColor.DarkGray);
+
+            DisplayColor($"{bollingerLow,7:C} ", hasLowEvent, lowEventColor);
+            DisplayColor($"{price.Low,7:C} ", hasLowEvent, lowEventColor);
+
+            Console.Write($"{ price.Close,7:C} ");
+
+            DisplayColor($"{price.High,7:C} ", hasHighEvent, highEventColor);
+            DisplayColor($"{bollingerHigh,7:C}", hasHighEvent, highEventColor);
+            Console.WriteLine();
+        }
+
+        private static void DisplayColor(string s, bool useColor, ConsoleColor color)
+        {
+            if (useColor) Console.ForegroundColor = color;
+            Console.Write(s);
+            if(useColor) Console.ResetColor();
         }
 
         private static Parameters LoadParameters(string filePath)
